e9d011f71b5cf9270f8e1828569b8ce136d9e31a
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-01 22:22:56 -0400

    initial design start

---
+++ script.js
@@ -0,0 +1,26 @@
+const nameList = document.querySelector('.name');
+const heightList = document.querySelector('.height');
+const hairColorList = document.querySelector('.hairColor');
+const skinColorList = document.querySelector('.skinColor');
+const genderList = document.querySelector('.gender');
+
+fetch('https://swapi.co/api/people/20/')
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		console.log(data);
+		const name = data.name;
+		const height = data.height;
+		const hairColor = data.hair_color;
+		const skinColor = data.skin_color;
+		const gender = data.gender;
+
+		nameList.innerText = `Hello, ${name}, my name is.`;
+		heightList.innerText = `Height: ${height} cm`;
+		hairColorList.innerText = `Hair color: ${hairColor}`;
+		skinColorList.innerText = `Skin color: ${skinColor}`;
+		genderList.innerText = `Gender: ${gender}`;
+
+		return fetch(date.homeworld);
+	});
